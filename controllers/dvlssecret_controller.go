a50d9425652dbe7994017dbb5f64141be2467551
Dion Gionet Mallet <dgionetmallet@devolutions.net>
2023-01-27 15:50:41 -0500

    feat: initial release

---
+++ controllers/dvlssecret_controller.go
@@ -0,0 +1,187 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	dvlsv1alpha1 "github.com/Devolutions/devolutions-kubernetes-operator/api/v1alpha1"
+
+	"github.com/Devolutions/go-dvls"
+)
+
+var (
+	DvlsClient      dvls.Client
+	RequeueDuration time.Duration
+)
+
+const (
+	DefaultRequeueDuration time.Duration = time.Minute
+
+	dvlsSecretType string = "devolutions.com/dvlssecret"
+
+	statusAvailableDvlsSecret string = "Available"
+	statusDegradedDvlsSecret  string = "Degraded"
+)
+
+// DvlsSecretReconciler reconciles a DvlsSecret object
+type DvlsSecretReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=dvls.devolutions.com,resources=dvlssecrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=dvls.devolutions.com,resources=dvlssecrets/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=dvls.devolutions.com,resources=dvlssecrets/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the DvlsSecret object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
+func (r *DvlsSecretReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	dvlsSecret := &dvlsv1alpha1.DvlsSecret{}
+	err := r.Get(ctx, req.NamespacedName, dvlsSecret)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Info("DvlsSecret object not found. Ignoring event since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, fmt.Errorf("failed to get DvlsSecret object, %w", err)
+	}
+
+	if dvlsSecret.Status.Conditions == nil || len(dvlsSecret.Status.Conditions) == 0 {
+		meta.SetStatusCondition(&dvlsSecret.Status.Conditions, v1.Condition{Type: statusAvailableDvlsSecret, Status: v1.ConditionUnknown, Reason: "Reconciling"})
+		if err := r.Status().Update(ctx, dvlsSecret); err != nil {
+			return ctrl.Result{}, fmt.Errorf("failed to update DvlsSecret status, %w", err)
+		}
+
+		err := r.Get(ctx, req.NamespacedName, dvlsSecret)
+		if err != nil {
+			return ctrl.Result{}, fmt.Errorf("failed to get DvlsSecret object, %w", err)
+		}
+	}
+
+	secret, err := DvlsClient.GetSecret(dvlsSecret.Spec.EntryID)
+	if err != nil {
+		log.Error(err, "unable to fetch dvls secret", "entry-id", dvlsSecret.Spec.EntryID)
+		meta.SetStatusCondition(&dvlsSecret.Status.Conditions, v1.Condition{Type: statusDegradedDvlsSecret, Status: v1.ConditionTrue, Reason: "Reconciling", Message: "Unable to fetch secret on DVLS instance"})
+		if err := r.Status().Update(ctx, dvlsSecret); err != nil {
+			log.Error(err, "Failed to update DvlsSecret status")
+		}
+		return ctrl.Result{}, nil
+	}
+	secretMap := make(map[string]string)
+	secretMap["entry-id"] = secret.ID
+	secretMap["username"] = secret.Username
+	secretMap["password"] = secret.Password
+
+	kSecret := &corev1.Secret{}
+	err = r.Get(ctx, req.NamespacedName, kSecret)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Info("Kubernetes secret not found, creating")
+			kSecret.ObjectMeta = v1.ObjectMeta{
+				Name:      req.Name,
+				Namespace: req.Namespace,
+			}
+			err := ctrl.SetControllerReference(dvlsSecret, kSecret, r.Scheme)
+			if err != nil {
+				return ctrl.Result{}, fmt.Errorf("failed to set kubernetes secret owner, %w", err)
+			}
+
+			kSecret.Type = corev1.SecretType(dvlsSecretType)
+			kSecret.StringData = secretMap
+
+			err = r.Create(ctx, kSecret)
+			if err != nil {
+				return ctrl.Result{}, fmt.Errorf("failed to create kubernetes secret, %w", err)
+			}
+
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, fmt.Errorf("failed to get kubernetes secret object, %w", err)
+	}
+
+	var owned bool
+	kSecretOwner := kSecret.GetOwnerReferences()
+	for _, v := range kSecretOwner {
+		if v.UID == dvlsSecret.GetUID() {
+			owned = true
+		}
+	}
+
+	if kSecret.Type != corev1.SecretType(dvlsSecretType) || !owned {
+		return ctrl.Result{}, fmt.Errorf("found existing kubernetes secret with name %s in namespace %s but is either not the correct type or not owned by the DvlsSecret resource. Either delete the existing secret or use a different name", kSecret.GetName(), kSecret.GetNamespace())
+	}
+
+	kSecretDataMapString := make(map[string]string)
+	for k, v := range kSecret.Data {
+		kSecretDataMapString[k] = string(v)
+	}
+
+	if !reflect.DeepEqual(kSecretDataMapString, secretMap) {
+		kSecret.StringData = secretMap
+		err := r.Update(ctx, kSecret)
+		if err != nil {
+			return ctrl.Result{}, fmt.Errorf("failed to get update kubernetes secret object, %w", err)
+		}
+
+		log.Info("updated secret")
+	}
+
+	meta.SetStatusCondition(&dvlsSecret.Status.Conditions, v1.Condition{Type: statusAvailableDvlsSecret, Status: v1.ConditionTrue, Reason: "Reconciling"})
+	meta.RemoveStatusCondition(&dvlsSecret.Status.Conditions, statusDegradedDvlsSecret)
+	if err := r.Status().Update(ctx, dvlsSecret); err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to update DvlsSecret status, %w", err)
+	}
+
+	return ctrl.Result{
+		RequeueAfter: RequeueDuration,
+	}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *DvlsSecretReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&dvlsv1alpha1.DvlsSecret{}).
+		Owns(&corev1.Secret{}).
+		Complete(r)
+}
---
+++ main.go
@@ -0,0 +1,149 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	dvlsv1alpha1 "github.com/Devolutions/devolutions-kubernetes-operator/api/v1alpha1"
+	"github.com/Devolutions/devolutions-kubernetes-operator/controllers"
+	"github.com/Devolutions/go-dvls"
+	//+kubebuilder:scaffold:imports
+)
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	utilruntime.Must(dvlsv1alpha1.AddToScheme(scheme))
+	//+kubebuilder:scaffold:scheme
+}
+
+func main() {
+	var metricsAddr string
+	var enableLeaderElection bool
+	var probeAddr string
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+	opts := zap.Options{
+		Development: true,
+	}
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   9443,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "8ffd49e3.devolutions.com",
+		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
+		// when the Manager ends. This requires the binary to immediately end when the
+		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
+		// speeds up voluntary leader transitions as the new leader don't have to wait
+		// LeaseDuration time first.
+		//
+		// In the default scaffold provided, the program ends immediately after
+		// the manager stops, so would be fine to enable this option. However,
+		// if you are doing or is intended to do any operation such as perform cleanups
+		// after the manager stops then its usage might be unsafe.
+		// LeaderElectionReleaseOnCancel: true,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	if err = (&controllers.DvlsSecretReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DvlsSecret")
+		os.Exit(1)
+	}
+	//+kubebuilder:scaffold:builder
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	appId, appIdSet := os.LookupEnv("DEVO_OPERATOR_DVLS_APPID")
+	appSecret, appSecretSet := os.LookupEnv("DEVO_OPERATOR_DVLS_APPSECRET")
+	dvlsBaseUri, dvlsBaseUriSet := os.LookupEnv("DEVO_OPERATOR_DVLS_BASEURI")
+	if !appIdSet || !appSecretSet || !dvlsBaseUriSet {
+		setupLog.Error(nil, "environment variables DEVO_OPERATOR_DVLS_APPID, DEVO_OPERATOR_DVLS_APPSECRET and DEVO_OPERATOR_DVLS_BASEURI must be set")
+		os.Exit(1)
+	}
+
+	dvlsClient, dvlsUser, err := dvls.NewClient(appId, appSecret, dvlsBaseUri)
+	if err != nil {
+		setupLog.Error(err, "unable to set up dvls client")
+		os.Exit(1)
+	}
+
+	if dvlsUser.UserType != dvls.UserAuthenticationApplication {
+		setupLog.Error(nil, "provided credentials are not for an Application user type", "userType", dvlsUser.UserType)
+		os.Exit(1)
+	}
+
+	requeueDurationString := os.Getenv("DEVO_OPERATOR_REQUEUE_DURATION")
+	if requeueDurationString != "" {
+		requeueDuration, err := time.ParseDuration(requeueDurationString)
+		if err != nil {
+			setupLog.Error(err, "invalid requeue duration")
+			os.Exit(1)
+		}
+		controllers.RequeueDuration = requeueDuration
+	} else {
+		controllers.RequeueDuration = controllers.DefaultRequeueDuration
+	}
+	controllers.DvlsClient = dvlsClient
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
